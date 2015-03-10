f8f5556aa3a59c58e3d847cc6bb26211ddd70f46
Mark Palmeri <mlp6@duke.edu>
2015-03-10 18:54:41 -0400

    add LICENSE to header and clean up comments

--- software/ADPL_complete/ADPL_complete.ino
+++ software/ADPL_complete/ADPL_complete.ino
@@ -1,6 +1,32 @@
-//This code includes all componenets - temp probes, level sensor, and relays
-//General overview of goals: Run pump based on tank level, operate burner based on temp3
-//SD card data collection commented out b/c connected to damaged Arduino
+/* ADPL_complete.ino
+
+This code includes all componenets - temp probes, level sensor, and relays
+General overview of goals: Run pump based on tank level, operate burner based
+on temp3 
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Aaron Forbis-Stokes and Mark Palmeri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 
 //Code for SD card
 //#include <SPI.h>
@@ -11,7 +37,6 @@
 ////for SD card
 //const int chipSelect = 10;   //pin 10 for SD card
 //#define WAIT_TO_START 0      //start readings immediately
-
 
 // temperature (thermistor) variables
 #define PROBE1 A0 
@@ -144,6 +169,7 @@ void loop() {
         }
     }   
  
+    // SD card data collection commented out b/c connected to damaged Arduino
     // writeSDcard(TempProbe[0]);
 
 } // end loop()

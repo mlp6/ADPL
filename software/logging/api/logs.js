5e79ef7ff7885293438cc858825afbe65155fe3c
Suyash Kumar <suyash.kumar@duke.edu>
2016-04-17 21:18:46 -0400

    Implemented GET records by location and GET records by location and pin

--- software/logging/api/logs.js
+++ software/logging/api/logs.js
@@ -7,8 +7,19 @@ module.exports = {
 			if(err) res.send(err);
 			res.json(records); 
 		});
+	},
+	listLoc: function(req,res){
+		LogEvent.find({loc:req.params.loc}, function(err,records){
+			if(err) res.send(err);
+			res.json(records);
+		});	
 
-	}
-
+	},
+	listPin: function(req, res){
+		LogEvent.find({probeid:req.params.pin, loc:req.params.loc}, function(err,records){
+			if(err) res.send(err);
+			res.json(records);
+		});	
+	} 
 
 }
--- software/logging/config/routes.js
+++ software/logging/config/routes.js
@@ -6,4 +6,6 @@ var express = require('express');
 var logs = require('../api/logs.js');
 module.exports = function(app) { 
 	app.get('/api/list', logs.list);
+	app.get('/api/list/:loc', logs.listLoc); 
+	app.get('/api/list/:loc/:pin', logs.listPin);
 };

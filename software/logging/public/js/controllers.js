9d02aa6dbc23f152878a945807cea912e294c9d0
Suyash Kumar <suyash.kumar@duke.edu>
2016-04-20 02:25:52 -0400

    Sliding scale implemented, annoying scale rendering problems solved. Closes #33

--- software/logging/public/js/controllers.js
+++ software/logging/public/js/controllers.js
@@ -32,38 +32,51 @@ angular.module('adplApp', ['ngMaterial', 'ngMessages'])
 	}
 
 }]);
+function getInnerWidth(elem) {
+    return parseFloat(window.getComputedStyle(document.getElementById(elem)).width);
+}
 
 var updateGraph = function(data){
+  var offset = 100;
 	nv.addGraph(function() {
-  var chart = nv.models.lineChart()
-                .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
-                .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!  
-                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-                .showYAxis(true)        //Show the y-axis
-                .showXAxis(true)        //Show the x-axis
-  ;
+  var chart = nv.models.lineWithFocusChart()
+  .width(getInnerWidth('graph')-offset)
+  .height(500);
 
-  chart.xAxis     //Chart x-axis settings
+  chart.xAxis     //Chart x-axis settings 
+      .axisLabel('Time and Date') 
+		.tickFormat(function(d) { return d3.time.format('%x %X')(new Date(d)); }) ;
+
+
+  chart.x2Axis     //Chart x-axis settings
       .axisLabel('Time and Date')
 		.tickFormat(function(d) { return d3.time.format('%x %X')(new Date(d)); });
-	
 
+	
 chart.xScale(d3.time.scale.utc());
+
   chart.yAxis     //Chart y-axis settings
       .axisLabel('Temperature (C)')
       .tickFormat(d3.format('.02f'));
 
+  chart.y2Axis     //Chart y-axis settings
+      .axisLabel('Temp (C)')
+      .tickFormat(d3.format('.02f')) 
+
+
+
+
   /* Done setting the chart up? Time to render it!*/
  
 
-  d3.select('#pulse svg')    //Select the <svg> element you want to render the chart in.   
-      .datum(data)         //Populate the <svg> element with chart data...
-      .call(chart)          //Finally, render the chart!
-	  .style({ 'width': "100%", 'height': 300 });
-d3.select('#pulse svg').datum(data).transition().duration(500).call(chart).style({ 'width': "100%", 'height': 300 });
+  d3.select('#graph svg')    //Select the <svg> element you want to render the chart in.   
+      .datum(data)         //Populate the <svg> element with chart data...  
+	  .style({ 'width': getInnerWidth('graph'), 'height': 500, 'margin-left':50 }) 
+      .call(chart);          //Finally, render the chart!
+//d3.select('#graph svg').datum(data).transition().duration(500).call(chart)
 	
   //Update the chart when window resizes.
-  nv.utils.windowResize(function() { chart.update() });
+  nv.utils.windowResize( chart.update() );
   return chart;
 });
 }

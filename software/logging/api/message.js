dd48fd93e6807f3094667ddf8fc7b50bb66dd5b4
Suyash Kumar <suyash.kumar@duke.edu>
2016-04-20 16:30:33 -0400

    Added test twilio integration that texts back latest probe 10 temp measurement!

---
+++ software/logging/api/message.js
@@ -0,0 +1,28 @@
+var LogEvent = require('../models/LogEvent.js');
+var twilio_auth = require('./twilio_auth.js');
+
+module.exports = {
+	onText: function(req,res){
+		if(req.body.Body == 'temp'){
+			// Get latest data for probe 10 and send text back to number 
+			LogEvent.find().sort('-time').find({probeid:10}, function(err, records){ 
+				sendText(records[1], req);
+				res.send("OK");
+			});
+			
+		}
+
+	}
+
+}
+function sendText(data, req){
+	var client = require('twilio')(twilio_auth.sid, twilio_auth.auth);
+	client.messages.create({
+		body: "Latest Probe "+data.probeid+" temp in "+ data.loc+": " +data.temp+"C at "+data.time,
+		to: req.body.From,
+		from: req.body.To}, function(err, message){
+		console.log(err);
+		console.log(message)});
+
+
+}
--- software/logging/config/routes.js
+++ software/logging/config/routes.js
@@ -4,8 +4,10 @@
 
 var express = require('express');
 var logs = require('../api/logs.js');
+var message = require('../api/message.js'); 
 module.exports = function(app) { 
 	app.get('/api/list', logs.list);
 	app.get('/api/list/:loc', logs.listLoc); 
 	app.get('/api/list/:loc/:pin', logs.listPin);
+	app.post('/api/message', message.onText);
 };

268e50c41c4fe7871f4e3ceb19357dc50c8572f8
Suyash Kumar <suyash.kumar@duke.edu>
2016-04-26 17:32:39 -0400

    Added Durham Location

--- software/logging/handle-device.js
+++ software/logging/handle-device.js
@@ -1,7 +1,8 @@
 var EventSource = require('eventsource'); // Pull in event source
 var LogEvent = require('./models/LogEvent.js');
 var locMap = {
-	"1b0048000c51343334363138": "kenya-1"
+	"1b0048000c51343334363138": "kenya-1",
+	"530031000a51343334363138": "durham-1"
 }; // Maps coreids to a location identifier 
 
 module.exports = function(deviceUrl, io){

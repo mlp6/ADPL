960ceef35da7ac6da2b9a90fc27954ff0540537e
Suyash Kumar <suyash.kumar@duke.edu>
2016-04-26 18:52:29 -0400

    Database overhaul, implemented site get/creation endpoints

--- software/logging/config/db.js
+++ software/logging/config/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var config = {
-  url: process.env.MONGO_URI || 'mongodb://localhost/adpl'
+  url: process.env.MONGO_URI || 'mongodb://localhost/adpl5'
 }; // The default port of MongoDB is 27017
 
 module.exports = function() {
--- software/logging/config/routes.js
+++ software/logging/config/routes.js
@@ -5,9 +5,13 @@
 var express = require('express');
 var logs = require('../api/logs.js');
 var message = require('../api/message.js'); 
+var sites = require('../api/sites.js'); 
+
 module.exports = function(app) { 
 	app.get('/api/list', logs.list);
 	app.get('/api/list/:loc', logs.listLoc); 
 	app.get('/api/list/:loc/:pin', logs.listPin);
+	app.get('/api/newsite/:locStr/:coreid',sites.newSite);
+	app.get('/api/sites',sites.listSites);
 	app.post('/api/message', message.onText);
 };

cbae32648348c61115f2aadef205823988a2a31d
Mark Palmeri <mlp6@duke.edu>
2016-05-18 09:00:41 -0400

    add new Durham-2 device (Durham-1 was damaged)

--- software/logging/config/device-map.js
+++ software/logging/config/device-map.js
@@ -2,5 +2,6 @@ module.exports={
 	locMap : {
 		"1b0048000c51343334363138": "kenya-1",
 		"530031000a51343334363138": "durham-1"
+		"440032000a51343334363138": "durham-2"
 	} // Maps coreids to a location identifier
 }

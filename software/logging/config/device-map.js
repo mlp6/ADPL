1d6eff88e2fd215ebb8a2df0cb3cff963388053c
Mark Palmeri <mlp6@duke.edu>
2016-05-18 11:02:55 -0400

    update device map lab for lab device

--- software/logging/config/device-map.js
+++ software/logging/config/device-map.js
@@ -2,6 +2,6 @@ module.exports={
 	locMap : {
 		"1b0048000c51343334363138": "kenya-1",
 		"530031000a51343334363138": "durham-1",
-		"440032000a51343334363138": "durham-2"
+		"440032000a51343334363138": "ADPLdukelab4601"
 	} // Maps coreids to a location identifier
 }

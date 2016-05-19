cb39352ccb9f94b72a972d17752c3d7fc779b12d
Suyash Kumar <suyash.kumar@duke.edu>
2016-05-19 00:28:40 -0400

    Adding in ondemand.js

---
+++ software/logging/api/ondemand.js
@@ -0,0 +1,20 @@
+var request = require("request"); 
+var deviceMap = require("../config/device-map.js");
+module.exports = {
+	getValveStatus: function(req,res){
+		var revMap = deviceMap.locMapRev();
+		var coreid = revMap[req.params.loc]; 
+		request("https://api.particle.io/v1/devices/"+coreid+"/gasOn?access_token=7883544edea996822936af401fad4209c2ba5627", function(error,response,body){
+			if (error) console.log(error);
+			console.log(body);
+			var parsed = JSON.parse(body);
+			if (parsed.hasOwnProperty("error")){
+				res.send("error");
+			}
+			res.json(parsed.result);
+		});
+
+
+	}
+
+}

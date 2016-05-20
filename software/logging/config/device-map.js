7fd57b872056bb6acb5c9467467ab7f6a7387729
Mark Palmeri <mlp6@duke.edu>
2016-05-20 09:33:42 -0400

    rename kenya-1 to ADPLKenyaNorth0437

--- software/logging/config/device-map.js
+++ software/logging/config/device-map.js
@@ -5,7 +5,7 @@
  */
 module.exports={
 	locMap : {
-		"1b0048000c51343334363138": "kenya-1",
+		"1b0048000c51343334363138": "ADPLKenyaNorth0437",
 		// "530031000a51343334363138": "durham-1",
 		"440032000a51343334363138": "ADPLdukelab4601"
 	}, // Maps coreids to a location identifier

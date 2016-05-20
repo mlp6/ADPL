dcaddf5697e3c29f1279574cf071d0458ea8516d
Mark Palmeri <mlp6@duke.edu>
2016-05-20 09:55:22 -0400

    move pin mapping preprocessor directives to separate header file

--- software/ADPL_electron/ADPL_electon.ino
+++ software/ADPL_electron/ADPL_electon.ino
@@ -12,18 +12,7 @@
 
 #define PUBLISH_DELAY 150000  // 2.5 min b/w variable publish
 
-// Define GPIO pins (Particle Electron) by function
-#define HXCI A0 // Heat Exchanger Cold Inlet
-#define HXCO A1 // Heat Exchanger Cold Output
-#define HTR A2  // Heater
-#define HXHI A3 // Heat Exchanger Hot Inlet
-#define HXHO A4 // Heat Exchanger Hot Outlet
-#define PUMP 7
-#define VALVE 4
-#define IGNITOR 2
-#define LEVEL B5
-#define BUCKET C0
-
+#include "pin_mapping.h"
 
 #include "TempProbe.h"
 TempProbe tempHXCI("HXCI", HXCI);
---
+++ software/ADPL_electron/pin_mapping.h
@@ -0,0 +1,12 @@
+// Define GPIO pins (Particle Electron) by function
+#define HXCI A0 // Heat Exchanger Cold Inlet
+#define HXCO A1 // Heat Exchanger Cold Output
+#define HTR A2  // Heater
+#define HXHI A3 // Heat Exchanger Hot Inlet
+#define HXHO A4 // Heat Exchanger Hot Outlet
+#define PUMP 7
+#define VALVE 4
+#define IGNITOR 2
+#define LEVEL B5
+//#define BUCKET C0 // Duke
+#define BUCKET C4 // Kenya

4873a182919826e3be70d97dd45ac51679b5dd5c
Suyash Kumar <suyash.kumar@duke.edu>
2016-08-31 02:47:53 -0400

    Updated twilo for remote text message monitoring

--- software/logging/api/message.js
+++ software/logging/api/message.js
@@ -8,11 +8,14 @@ var LogEvent = require('../models/LogEvent.js');
 var twilio_auth = require('./twilio_auth.js');
 
 module.exports = {
-	onText: function(req,res){
-		if(req.body.Body == 'temp'){
+	onText: function(req,res){ 
+        var wordArray = req.body.Body.split(' ');
+		if(wordArray[0] == 'temp'){
+            var location = (wordArray.length > 1) ? wordArray[1] : 'ADPLDukelab0278';
 			// Get latest data for probe 10 and send text back to number 
-			LogEvent.find().sort('-time').find({probeid:10}, function(err, records){ 
-				sendText(records[1], req);
+			LogEvent.find().sort('-time').find({loc: location}, function(err, records){ 
+                console.log(records.slice(0,5));
+				sendText(records.slice(0,5), req);
 				res.send("OK");
 			});
 			
@@ -23,8 +26,13 @@ module.exports = {
 }
 function sendText(data, req){
 	var client = require('twilio')(twilio_auth.sid, twilio_auth.auth);
+    var createTempString = function(item) {
+        return `${item.probeid}: ${item.temp} C`;
+    };
+    var tempStrings = data.map(createTempString).join("\n");
+    console.log(tempStrings);
 	client.messages.create({
-		body: "Latest Probe "+data.probeid+" temp in "+ data.loc+": " +data.temp+"C at "+data.time,
+        body: `Latest Probe temps in ${data[1].loc} @ ${data[1].time}:\n${tempStrings}`,
 		to: req.body.From,
 		from: req.body.To}, function(err, message){
 		console.log(err);
--- software/logging/config/device-map.js
+++ software/logging/config/device-map.js
@@ -10,7 +10,8 @@ module.exports={
 		"440032000a51343334363138": "ADPLdukelab4601",
 		"5a004e001751353338363036": "ADPLDukelab0278",
 		"310057000951343334363138": "ADPLPhilippinesSagay3904",
-		"170044000c51343334363138": "ADPLPhilippinesSubayon1817"
+		"170044000c51343334363138": "ADPLPhilippinesSubayon1817",
+        "430055000951343334363138": "ADPLIndia3847"
 	}, // Maps coreids to a location identifier
 	locMapRev : function(){ 
 		revMap = {};

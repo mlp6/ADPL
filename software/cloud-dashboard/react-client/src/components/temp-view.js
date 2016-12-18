28e180645a7c2197d8c36a1ccc1910428a98c537
Suyash Kumar <suyash.kumar@duke.edu>
2016-12-18 00:34:15 -0800

    Added initial temps graph, built app-wide redux loading middleware

--- software/cloud-dashboard/react-client/src/components/temp-view.js
+++ software/cloud-dashboard/react-client/src/components/temp-view.js
@@ -1,15 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Card, CardTitle } from 'react-toolbox/lib/card';
+import ProgressBar from 'react-toolbox/lib/progress_bar';
+import TempPlot from './temp-view/temp-plot';
+
 class TempView extends Component {
-	render() {
+
+	renderLoadingView = () => {
 		return (
-			<Card style={{minHeight: '200px'}}>
-				<CardTitle
-					title="Metadata"
-					subtitle="This is cool"/>
-			</Card>
+			<div style={{textAlign: 'center'}}>
+				<ProgressBar type="circular" mode="indeterminate"/>
+			</div>
+		)
+	} 
+
+	render() {
+		return ( 
+			<div>
+				<Card style={{minHeight: '100px'}}>
+					<CardTitle
+						title="Metadata"/> 
+					{
+						this.props.temps.loading && 
+						this.renderLoadingView()
+					}
+				</Card>
+				<Card style={{minHeight: '400px', marginTop: '20px'}}> 
+					<CardTitle
+						title="Temperature Data"
+						subtitle="Last 3 Days"/>
+					{
+						this.props.temps.loading && 
+						this.renderLoadingView()
+					}
+					<TempPlot temps={this.props.temps}/>
+				</Card>
+			</div>
 		);
 	}
+}
+
+TempView.propTypes ={
+	temps: PropTypes.object.isRequired
 }
 
 export default TempView;

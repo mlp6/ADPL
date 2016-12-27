cd1721a0825e3e9b63ebdf376b3ac1a4fa9bed9f
Suyash Kumar <suyash.kumar@duke.edu>
2016-12-27 01:40:57 -0500

    Added bucket handling db schema, stream parsing, and API routes

--- software/cloud-dashboard/node-server/config/routes.js
+++ software/cloud-dashboard/node-server/config/routes.js
@@ -5,16 +5,22 @@
  */
 
 var express = require('express');
-var logs = require('../api/logs.js');
-var message = require('../api/message.js'); 
-var sites = require('../api/sites.js');
-var ondemand = require('../api/ondemand.js');
+var temperatureHandlers = require('../api/temperature-handlers');
+var bucketHandlers = require('../api/bucket-handlers');
+var message = require('../api/message'); 
+var sites = require('../api/sites');
+var ondemand = require('../api/ondemand');
+
 module.exports = function(app) { 
-	app.get('/api/list', logs.list);
-	app.get('/api/list/:loc', logs.listLoc); 
-	app.get('/api/list-all/:loc',logs.listLocAll);
-	app.get('/api/list/:loc/:pin', logs.listPin);
+	app.get('/api/list', temperatureHandlers.list);
+	app.get('/api/list/:loc', temperatureHandlers.listLoc); 
+	app.get('/api/list-all/:loc',temperatureHandlers.listLocAll);
+	app.get('/api/list/:loc/:pin', temperatureHandlers.listPin);
 	app.get('/api/sites',sites.listSites);
 	app.get('/api/gasOn/:loc',ondemand.getValveStatus);
 	app.post('/api/message', message.onText);
+
+	// Bucket Endpoints
+	app.get('/api/bucket/list', bucketHandlers.list);
+	app.get('/api/bucket/list/:loc', bucketHandlers.listLoc);
 };
--- software/cloud-dashboard/node-server/handle-device.js
+++ software/cloud-dashboard/node-server/handle-device.js
@@ -6,80 +6,33 @@
  * @author: Suyash Kumar <suyashkumar2003@gmail.com>
  */
 var EventSource = require('eventsource'); // Pull in event source
-var TemperatureEvent = require('./models/temperature-event');
-var locMap = require('./config/device-map.js').locMap; 
-
-function addRecord(parsedData, io){
-	var newTemperatureEvent= {
-		coreid:		parsedData.coreid,
-		time:		new Date(parsedData.published_at),
-		loc:		locMap[parsedData.coreid], 
-		temps:		{
-			HXCI:	parsedData.HXCI,
-			HXCO:	parsedData.HXCO,
-			HTR:	parsedData.HTR,
-			HXHI:	parsedData.HXHI,
-			HXHO:	parsedData.HXHO
-		},
-		valveStatus: parsedData.V // Null if doesn't exist
-	}
-	
-	console.log(newTemperatureEvent);
-	var newRecord = new TemperatureEvent(newTemperatureEvent);  
-	newRecord.save(function(err, event){
-		if(err) console.log("error in saving to database"+err);
-	})
-	io.emit(newTemperatureEvent.probeid, newRecord);
-}
-
-/**
- * parseMessage
- * Responsible for parsing a Particle Publish 
- * message from the ADPL platform. A sample message
- * looks like:
- * {
- * 	"data": {
- * 		"data":"HXCI:18.9,HXCO:21.8,HTR:80.6,HXHI:22.7,HXHO:19.0",
- * 		""ttl":"60",
- * 		"published_at":"2016-12-02T06:18:25.026Z",
- * 		"coreid":"4b0031000d51353432393339"
- * 	}
- * }
- * This function returns a map of all the transducer fields to 
- * corresponding values along with published_at and coreid 
- * (e.g. {HXHO: 100, ... , coreid: "", published_at: ""})
- * @param {object} message
- * @return {object} a map of data fields to their values (including transducers) 
- */
-function parseMessage(message) {
-	deviceData = JSON.parse(message.data); 
-	const splitData = deviceData.data.split(",");
-	
-	// Parse Transducer data
-	const parsedData = splitData.reduce((result, current) => {
-		const currentSplit = current.split(":");
-		result[currentSplit[0]] = currentSplit[1];
-		return result;
-	}, {}); 
-	
-	// Add metadata
-	parsedData['published_at'] = deviceData['published_at'];
-	parsedData['coreid'] = deviceData['coreid'];
-
-	return parsedData;
-}
+var TempUtil = require('./handle-device/temp-util');
+var BucketUtil = require('./handle-device/bucket-util');
 
 function handleDevice(deviceUrl, io){
 	var es = new EventSource(deviceUrl); // Listen to the stream
+
+	// Add temperature event listener
     es.addEventListener("TEMPS", function (message) {
         console.log("New Message");
 		try {
-  			parsedData = parseMessage(message); 
-			addRecord(parsedData, io);
+  			const parsedData = TempUtil.parseMessage(message); 
+			TempUtil.addRecord(parsedData, io);
 		} catch (err) {
 			console.log("ERROR: Parsing Temps Message", message, err);	
 		}
     });
+
+	// Add bucket tip listener
+	es.addEventListener("BUCKET", function(message) {
+		try {
+			const parsedData = BucketUtil.parseMessage(message);
+			BucketUtil.addRecord(parsedData, io);
+		} catch (err) {
+			console.log("ERROR: Parsing BUCKET Message", message, err);
+		}
+
+	});
 
     es.onerror = function (err) {
         console.log("ERROR (Likely Event Source)");

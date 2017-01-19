0dc5322f9273ab08b4a1dc6edcf2b5871ac18de4
Suyash Kumar <suyash.kumar@duke.edu>
2017-01-19 00:14:12 -0800

    Added additional tests

--- software/cloud-dashboard/node-server/tests/handle-device/bucket-util.test.js
+++ software/cloud-dashboard/node-server/tests/handle-device/bucket-util.test.js
@@ -1,6 +1,11 @@
 var testData = require('./test-data');
 var sinon = require('sinon'); 
 
-describe('', () => {
-	
+var BucketUtil = require('../../handle-device/bucket-util');
+
+describe('BucketUtil Tests', () => {
+	const data = BucketUtil.parseMessage(testData.sampleBucketEvent);	
+	it('parseMessage returns the right data', () => {
+		expect(data).toEqual(testData.bucketEventParsedData);
+	});
 });

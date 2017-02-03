88cdba291ac78dfbff2c2344acd7f601235393a1
Mark Palmeri <mlp6@duke.edu>
2017-02-03 14:38:49 -0500

    update device ID map <-> name

--- software/cloud-dashboard/node-server/config/device-map.js
+++ software/cloud-dashboard/node-server/config/device-map.js
@@ -2,16 +2,20 @@
  * device-map.js
  * Holds mapping to/from coreid to internal loc identifier
  * @author: Suyash Kumar <suyashkumar2003@gmail.com>
+ *
+ * Updated (2017-02-03) by Mark Palmeri
  */
 module.exports={
 	locMap : {
-		"220036000b51343136333035": "ADPLKenyaC6304",
-        "3e001d000e51353432393339": "ADPLKenyaN3763",
+		"220036000b51343136333035": "ADPLKenyaC2212",
+		"4b0031000d51353432393339": "ADPLKenyaC6304",
+        "3e001d000e51353432393339": "ADPLKenyaN6833",
         "400057000a51343334363138": "ADPLDuke0785",
+        "20002a001951353338363036": "ADPLDuke6191",
 		"310057000951343334363138": "ADPLPhilippinesSagay3904",
 		"170044000c51343334363138": "ADPLPhilippinesSubayon1817",
         "430055000951343334363138": "ADPLIndia3847",
-        "2d0042000951343334363138": "ADPLIndia2723"
+        "2d0042000951343334363138": "ADPLindia2723"
 	}, // Maps coreids to a location identifier
 	locMapRev : function(){ 
 		revMap = {};

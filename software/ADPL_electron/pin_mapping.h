b533c41f46efae06a77dac36680dc66f9ce17499
Mark Palmeri <mlp6@duke.edu>
2017-06-07 21:21:29 -0400

    update pin mapping

--- software/ADPL_electron/pin_mapping.h
+++ software/ADPL_electron/pin_mapping.h
@@ -14,6 +14,10 @@
 #define DIR D5
 #define SLEEP D6
 #define UP B5
-#define DOWN B4
+#define DOWN C1
 #define RESET C2
 #define SD_CS_PIN B3
+//#define SD_CD_PIN B4
+//#define SD_DI_PIN B2
+//#define SD_DO_PIN B1
+//#define SD_CLK_PIN B0

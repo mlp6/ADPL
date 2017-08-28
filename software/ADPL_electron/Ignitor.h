db2df5eee5a9f3b42c330c58cbcd5d6a2c02cbe1
Mark Palmeri <mlp6@duke.edu>
2017-08-28 17:31:44 -0400

    add ignitor refire lockout (issue #254)

--- software/ADPL_electron/Ignitor.h
+++ software/ADPL_electron/Ignitor.h
@@ -10,7 +10,10 @@ class Ignitor {
         unsigned long timeLastFired;
         unsigned int repeatRefireAttempts = 0;
         unsigned int repeatRefireLimit = 5;
-        unsigned int refireDelay = 60000; // ms; delay between ignition attempts
+        unsigned long refireDelay = 60000; // ms; delay between ignition attempts
+        unsigned long resumeReignitionTime = 0; // ms; absolute time to reattempt ignition (set in main ino)
+        unsigned long resumeReignitionDelay = 3600000; // ms; wait an hour after repeatRefireLimit attemps to ignite
+        bool allow = true; // allow attempted ignition
     private:
         int _pin;
         static const int _ignitor_on_time = 10000;   // ms; keep on for 10 s

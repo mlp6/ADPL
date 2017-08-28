58ce9a75870cd2bf82ecae21713a3dc76a7f8ee0
Mark Palmeri <mlp6@duke.edu>
2017-08-28 17:04:21 -0400

    replace global IGNITOR_DELAY with ignitor.refireDelay attribute

--- software/ADPL_electron/Ignitor.h
+++ software/ADPL_electron/Ignitor.h
@@ -8,6 +8,9 @@ class Ignitor {
         Ignitor(int pin);
         void fire();
         unsigned long timeLastFired;
+        unsigned int repeatRefireAttempts = 0;
+        unsigned int repeatRefireLimit = 5;
+        unsigned int refireDelay = 60000; // ms; delay between ignition attempts
     private:
         int _pin;
         static const int _ignitor_on_time = 10000;   // ms; keep on for 10 s

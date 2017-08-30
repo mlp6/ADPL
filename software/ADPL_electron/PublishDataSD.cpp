2fe0523ac679bd88f480027ebf96e995f9a09020
Mark Palmeri <mark.palmeri@duke.edu>
2017-08-30 15:36:56 -0400

    fix epoch timestamp (#256)
    fix epoch timestamp writing (closes #253)

--- software/ADPL_electron/PublishDataSD.cpp
+++ software/ADPL_electron/PublishDataSD.cpp
@@ -4,23 +4,22 @@
 
 bool PublishDataSD::publish(double HXCI, double HXCO, double HTR, double HXHI, double HXHO,
                             int gasOn, int bucket_tip_count, File sdFile){
-    char data_str [69];
+    char data_str [79];
     int bitsWritten = -1;
     // bucket.tip_count will be ignored if not needed by sprintf
-    fmt_string_SD = "HXCI:%.1f,HXCO:%.1f,HTR:%.1f,HXHI:%.1f,HXHO:%.1f,V:%d,B:%d";
-    fmt_string_no_bucket_SD = "HXCI:%.1f,HXCO:%.1f,HTR:%.1f,HXHI:%.1f,HXHO:%.1f,V:%d";
+    fmt_string_SD = "[%d]HXCI:%.1f,HXCO:%.1f,HTR:%.1f,HXHI:%.1f,HXHO:%.1f,V:%d,B:%d";
+    fmt_string_no_bucket_SD = "[%d]HXCI:%.1f,HXCO:%.1f,HTR:%.1f,HXHI:%.1f,HXHO:%.1f,V:%d";
 
     sprintf(data_str, (bucket_tip_count > 0) ? fmt_string_SD : fmt_string_no_bucket_SD,
-            HXCI, HXCO, HTR, HXHI, HXHO, gasOn, bucket_tip_count);
+            Time.now(), HXCI, HXCO, HTR, HXHI, HXHO, gasOn, bucket_tip_count);
 
     // open the file for write at end like the "Native SD library"
     sdFile.open("adpl_data.txt", O_RDWR | O_CREAT | O_AT_END);
-    // if the file opened okay, write to it:
-    printTime(sdFile);
-    // print data
+
     bitsWritten = sdFile.println(data_str);
 
     sdFile.close();
+
     //Indicates publishing success
     if(bitsWritten > -1){
         return true;
@@ -38,7 +37,7 @@ bool PublishDataSD::pushToCell(File sdFile) {
     // initialize a bool to determine success
     bool success = true;
     // read each line in the file and subsequently publish it
-    char data_str[69];
+    char data_str[79];
     size_t n;
     while ((n = sdFile.fgets(data_str, sizeof(data_str))) > 0) {
         //determine if reading the end of a line
@@ -63,11 +62,9 @@ bool PublishDataSD::logError(int errorCode) {
         // if the file doesn't open
         return false;
     }
-    // if the file opened okay, write to it:
-    // print time
-    printTime(errorFile);
-    // print data
-    bitsWritten = errorFile.println(errorCode);
+    char error_str [69];
+    sprintf(error_str, "[%d]%d", Time.now(), errorCode);
+    bitsWritten = errorFile.println(error_str);
 
     errorFile.close();
     //Indicates publishing success
@@ -76,10 +73,4 @@ bool PublishDataSD::logError(int errorCode) {
     } else {
         return false;
     }
-}
-
-void PublishDataSD::printTime(File file) { // Prints a UTC timestamp
-    file.print("[");
-    file.print(Time.now());
-    file.print("]");
 }

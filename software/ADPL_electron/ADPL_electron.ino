5fadbdc348bb86806937e0fa6bbd3cb8f68902ea
Mark Palmeri <mark.palmeri@duke.edu>
2018-05-18 22:33:16 -0400

    [WIP] add exhaust temp to publish/save str (closes #267) (#288)
    * add exhaust temp to publish/save str (closes #267)
    
    * tempExhaust.temp is now published by cell
    
    * tempExhaust.temp is now saved to the SD card
    
    * note - this commit does not optionally publish/save the exhaust temp;
      if the exhaust temp is bogus, it will still consume published bytes
    
    * rm no_bucket data string option (closes #290)
    
    * fix variable input to SD save and char* cast to cell publish

--- software/ADPL_electron/ADPL_electron.ino
+++ software/ADPL_electron/ADPL_electron.ino
@@ -135,7 +135,8 @@ void loop() {
 
         if (Particle.connected()) { //Returns true if the device is connected to the cellular network
             if (cellPublisher.publish(tempHXCI.temp, tempHXCO.temp, tempHTR.temp, tempHXHI.temp,
-                                      tempHXHO.temp, int(valve.gasOn), int(bucket.tip_count))) {
+                                      tempHXHO.temp, tempExhaust.temp, int(valve.gasOn),
+                                      int(bucket.tip_count))) {
                 publishedCell = true;
             } else {
                 logError(CELL_PUB_FAIL);
@@ -159,7 +160,7 @@ void loop() {
             }
         } else if (SDCARD){ //only publish to SD card if there is no cell connection
             if(sdPublisher.publish(tempHXCI.temp, tempHXCO.temp, tempHTR.temp, tempHXHI.temp, tempHXHO.temp,
-                                   int(valve.gasOn), int(bucket.tip_count), sdFile)){
+                                   tempExhaust.temp, int(valve.gasOn), int(bucket.tip_count), sdFile)){
                 publishedSD = true;
             } else {
                 logError(SD_PUB_FAIL);
@@ -314,6 +315,6 @@ void down_pushed() {
 void logError(int errorCode) {
     sdPublisher.logError(errorCode);
     if(Particle.connected()){
-        Particle.publish("ERROR", errorCode);
+        Particle.publish("ERROR", (char*)errorCode);
     }
 }

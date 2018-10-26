d553ee101f0a1a0e24133657388f551ab2634fbc
Graham-H-Miller <graham.h.miller@duke.edu>
2018-10-26 13:22:34 -0400

    Update device-map.js

--- software/cloud-dashboard/node-server/config/device-map.js
+++ software/cloud-dashboard/node-server/config/device-map.js
@@ -4,8 +4,8 @@
  */
 module.exports={
 	locMap : {
-		"46005b000b51343334363138": "Kenya-Central",
-    "4a003b000351353337353037": "Kenya-North",
+		"3b001b000e51353432393339": "Kenya-Central",
+    "47003c000251353337353037": "Kenya-North",
     "230042001951353338363036": "Duke",
 		"390037000451353431383736": "Philippines-Sagay",
 		"350055000551353431383736": "Philippines-Subayon",
